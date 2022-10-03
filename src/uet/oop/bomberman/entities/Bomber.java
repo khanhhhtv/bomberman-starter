ca8eb8f4ed0fc70be19048b1cccf40f6bb9f2810
thanhttruc <21020414@vnu.edu.vn>
2022-10-03 23:27:10 +0700

    commit new

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -9,6 +9,10 @@ import javafx.scene.paint.Color;
 public class Bomber extends Animal {
 
     int i = 1;
+    public Bomber(int is_move, int swap, String direction, int count, int count_to_run) {
+        super(8, 1, "down", 0, 0);
+    }
+
     public Bomber(int x, int y, Image img) {
         super( x, y, img);
     }
